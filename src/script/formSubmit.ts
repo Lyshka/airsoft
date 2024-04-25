6a2dd9438b9f814547d9e1cb5184318707fd25d9
Lyshka <lesha_novitskiy@mail.ru>
2024-04-25 16:00:07 +0300

    finish

--- src/script/formSubmit.ts
+++ src/script/formSubmit.ts
@@ -1,9 +1,12 @@
+import axios from "axios";
+import { Response } from "../types/response";
+
 const allFormElements = [
   ...document.querySelectorAll("form"),
 ] as HTMLFormElement[];
 
 allFormElements.map((formElement) => {
-  formElement.onsubmit = (event) => {
+  formElement.onsubmit = async (event) => {
     event.preventDefault();
     const errorConfElement = formElement.querySelector(".errorConf");
 
@@ -40,6 +43,11 @@ allFormElements.map((formElement) => {
       phoneInputElement.classList.remove("error");
     }
 
+    const nameInput = (event.target as HTMLFormElement)?.elements?.namedItem(
+      "name"
+    ) as HTMLInputElement;
+    const nameValue = nameInput.value;
+
     const typeForm = formElement.dataset["typeForm"];
 
     if (typeForm === "requestCall") {
@@ -47,13 +55,57 @@ allFormElements.map((formElement) => {
         "modalRequestCallResponse"
       );
 
-      modalRequestCallResponse?.classList.add("open");
+      const {
+        data: { error, success },
+      } = await axios.post<Response>(
+        `${window.location.origin}/form.php`,
+        {
+          formName: typeForm,
+          tel: valueInput,
+          name: nameValue,
+          target: "Заказать звонок",
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+
+      if (success) {
+        modalRequestCallResponse?.classList.add("open");
+      } else {
+        console.error(error);
+      }
     } else {
       const modalSignUpResponse = document.getElementById(
         "modalSignUpResponse"
       );
 
-      modalSignUpResponse?.classList.add("open");
+      const {
+        data: { error, success },
+      } = await axios.post<Response>(
+        `${window.location.origin}/form.php`,
+        {
+          formName: typeForm,
+          tel: valueInput,
+          name: nameValue,
+          target: "Оставить заявку",
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+
+      if (success) {
+        modalSignUpResponse?.classList.add("open");
+      } else {
+        console.error(error);
+      }
     }
+
+    (event.target as HTMLFormElement).reset();
   };
 });
