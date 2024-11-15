631c0d69f694c43e599ca03eb340ac6f324260d5
Lyshka <53864647+Lyshka@users.noreply.github.com>
2024-11-15 09:01:00 +0300

    change chat_id

--- src/script/sendTelegram.ts
+++ src/script/sendTelegram.ts
@@ -4,7 +4,7 @@ export const sendTelegram = async (message: string) => {
   await axios.post(
     "https://api.telegram.org/bot7116905595:AAHI3nkekurzOGLKA3BqPVNJJ7c9BffBMOo/sendMessage",
     {
-      chat_id: 897695033,
+      chat_id: -4532825102,
       text: message,
       parse_mode: "html",
     },
